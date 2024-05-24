01744369f4a2c73a61ea96489350269051349e00
jejejekim <46785917+jejejekim@users.noreply.github.com>
2024-05-24 13:14:14 +0900

    Add files via upload

---
+++ 데이터조회/mainpage.js
@@ -0,0 +1,21 @@
+// function renderPost(){
+//     for({post.length})
+// }
+console.log({ post });
+
+function toggleSubcategory(id) {
+  var subcategory = document.getElementById(id);
+  subcategory.classList.toggle("active");
+}
+
+function redirectToPage() {
+  window.location.href = "detaildata.html";
+}
+
+function redirectToPage_mypage() {
+  window.location.href = "mypage.html";
+}
+
+function redirectToPage_add() {
+  window.location.href = "createdata.html";
+}
